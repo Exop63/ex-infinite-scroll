abc0d3bd72f7a7746b209697719f9fd95362021e
Abdullah BAĞALAN <abdulahbaglan@gmail.com>
2020-11-10 21:43:37 +0300

    forRoot() eklendi.

--- projects/ex-infinite-scroll/src/lib/ex-infinite-scroll.module.ts
+++ projects/ex-infinite-scroll/src/lib/ex-infinite-scroll.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { ExInfiniteScrollComponent } from './ex-infinite-scroll.component';
+import { ExInfiniteScrollConfig } from './ex-infinite-scroll.model';
 
 
 
@@ -9,4 +10,20 @@ import { ExInfiniteScrollComponent } from './ex-infinite-scroll.component';
   ],
   exports: [ExInfiniteScrollComponent]
 })
-export class ExInfiniteScrollModule { }
+export class ExInfiniteScrollModule {
+
+  constructor(@Optional() @SkipSelf() parentModule?: ExInfiniteScrollModule) {
+    if (parentModule) {
+      throw new Error(
+        'ExInfiniteScrollModule is already loaded. Import it in the AppModule only');
+    }
+  }
+
+
+  static forRoot(config: ExInfiniteScrollConfig): ModuleWithProviders < ExInfiniteScrollModule > {
+  return {
+    ngModule: ExInfiniteScrollModule,
+    providers: [{ provide: ExInfiniteScrollModule, useValue: config }]
+  }
+}
+}
